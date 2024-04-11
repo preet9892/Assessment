ba7cdaf1dfef9605681931c6535624a5277b1a05
preet9892 <166673953+preet9892@users.noreply.github.com>
2024-04-11 08:00:12 +0530

    Add files via upload

---
+++ turtlee.py
@@ -0,0 +1,17 @@
+from turtle import  Turtle, Screen
+baby = Turtle()
+baby.shape("turtle")
+baby.color("coral")
+
+
+def drawTriangle(x):
+    x.left(120)
+    x.forward(200)
+    x.left(120)
+    x.forward(200)
+    x.left(120)
+    x.forward(200)
+
+drawTriangle(baby)
+baby.position(-3,-3)
+drawTriangle(baby)
